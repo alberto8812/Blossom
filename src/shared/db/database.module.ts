8a65e92c9deb555a4f67c49ebd3ff6684b71306f
alberto8812 <cvelascosaavedra@gamil.com>
2024-12-20 21:27:28 -0500

    feat(db):Configurar conexión a la base de datos con Sequelize y variables de entorno; actualizar el puerto del servidor y agregar registro de inicio

---
+++ src/shared/db/database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { envs } from 'src/config/env';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: envs.host,  // Usamos directamente el objeto `envs`
+        port: envs.dbport,
+        username: envs.username,
+        password: envs.password,
+        database: envs.database,
+        models: [],  // Aquí incluyes todas las entidades que utilizarás
+        autoLoadModels: true,
+        synchronize: true,  // Establece esto en `false` para producción
+      }),
+    }),
+  ],
+})
+export class DatabaseModule { }
