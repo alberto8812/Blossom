cba2507b5dff8ddbd7a0de8b599909cce48c3e26
alberto8812 <cvelascosaavedra@gamil.com>
2024-12-22 22:30:46 -0500

    feat(gender, origin): add error handling and logging to gender and origin services

--- src/gender/aplication/gender-use-case.service.ts
+++ src/gender/aplication/gender-use-case.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, Logger } from '@nestjs/common';
 import { IResponse, UseCaseGenderService } from './gender-use-case.interface';
 import { GenderRepository } from '../domain/repository/gender.repository';
 import { CrudGenderRepository } from '../domain/repository/gender-repository.interface';
@@ -6,41 +6,64 @@ import { GenderRepositoryModel, ISaveGenderRepositoryModel } from '../domain/mod
 
 @Injectable()
 export class GenderService implements UseCaseGenderService {
+  private readonly logger = new Logger('GenderService');
   constructor(
     @Inject(GenderRepository) private GenderRepository: CrudGenderRepository
   ) {
 
   }
   async updateGenderById(updateCharcter: GenderRepositoryModel): Promise<any> {
-    const character = await this.GenderRepository.updateGender(updateCharcter);
-    return {
-      message: 'Character updated',
-      code: 200,
-      data: character
+    try {
+      const character = await this.GenderRepository.updateGender(updateCharcter);
+      return {
+        message: 'Character updated',
+        code: 200,
+        data: character
+      }
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException("Error updating character");
     }
   }
   async createGender(newCharacter: ISaveGenderRepositoryModel): Promise<any> {
-    const character = await this.GenderRepository.createGender(newCharacter);
-    return {
-      message: 'Character created',
-      code: 200,
-      data: character
+    try {
+      const character = await this.GenderRepository.createGender(newCharacter);
+      return {
+        message: 'Character created',
+        code: 200,
+        data: character
+      }
+
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException("Error creating character");
+
     }
   }
   async getAllGender(): Promise<IResponse<GenderRepositoryModel>> {
-    const character = await this.GenderRepository.getAllGender();
-    return {
-      message: 'Character list',
-      code: 200,
-      data: character
+    try {
+      const character = await this.GenderRepository.getAllGender();
+      return {
+        message: 'Character list',
+        code: 200,
+        data: character
+      }
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException("Error getting characters");
     }
   }
   async deleteGenderById(employeeid: string): Promise<IResponse<any>> {
-    const character = await this.GenderRepository.deleteGenderById(employeeid);
-    return {
-      message: 'Character deleted',
-      code: 200,
-      data: character
+    try {
+      const character = await this.GenderRepository.deleteGenderById(employeeid);
+      return {
+        message: 'Character deleted',
+        code: 200,
+        data: character
+      }
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException("Error deleting character");
     }
   }
   async getGenderById(characterId: string): Promise<IResponse<GenderRepositoryModel>> {
--- src/origin/aplication/origin-use-case.service.ts
+++ src/origin/aplication/origin-use-case.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, Logger } from '@nestjs/common';
 import { IResponse, UseCaseOriginService } from './origin-use-case.interface';
 import { OriginRepository } from '../domain/repository/origin.repository';
 import { CrudOriginRepository } from '../domain/repository/origin-repository.interface';
@@ -6,49 +6,78 @@ import { OriginRepositoryModel, ISaveOriginRepositoryModel } from '../domain/mod
 
 @Injectable()
 export class OriginService implements UseCaseOriginService {
+  private readonly logger = new Logger('OriginService');
   constructor(
     @Inject(OriginRepository) private originRepository: CrudOriginRepository
   ) {
 
   }
   async updateOriginById(updateOrigin: OriginRepositoryModel): Promise<any> {
-    const character = await this.originRepository.updateOrigin(updateOrigin);
-    return {
-      message: 'Character updated',
-      code: 200,
-      data: character
+    try {
+      const character = await this.originRepository.updateOrigin(updateOrigin);
+      return {
+        message: 'Character updated',
+        code: 200,
+        data: character
+      }
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException("Error updating character");
     }
   }
   async createOrigin(newOrigin: ISaveOriginRepositoryModel): Promise<any> {
-    const character = await this.originRepository.createOrigin(newOrigin);
-    return {
-      message: 'Character created',
-      code: 200,
-      data: character
+    try {
+      const character = await this.originRepository.createOrigin(newOrigin);
+      return {
+        message: 'Character created',
+        code: 200,
+        data: character
+      }
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException("Error creating character");
     }
   }
   async getAllOrigin(): Promise<IResponse<OriginRepositoryModel>> {
-    const character = await this.originRepository.getAllOrigin();
-    return {
-      message: 'Character list',
-      code: 200,
-      data: character
+    try {
+      const character = await this.originRepository.getAllOrigin();
+      return {
+        message: 'Character list',
+        code: 200,
+        data: character
+      }
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException("Error getting characters");
     }
+
   }
   async deleteOriginById(OriginId: string): Promise<IResponse<any>> {
-    const character = await this.originRepository.deleteOriginById(OriginId);
-    return {
-      message: 'Character deleted',
-      code: 200,
-      data: character
+    try {
+      const character = await this.originRepository.deleteOriginById(OriginId);
+      return {
+        message: 'Character deleted',
+        code: 200,
+        data: character
+      }
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException("Error deleting character");
     }
   }
   async getOriginById(OriginId: string): Promise<IResponse<OriginRepositoryModel>> {
-    const character = await this.originRepository.getOriginById(OriginId);
-    return {
-      message: 'Character found',
-      code: 200,
-      data: character
+    try {
+      const character = await this.originRepository.getOriginById(OriginId);
+      return {
+        message: 'Character found',
+        code: 200,
+        data: character
+      }
+
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException("Error getting character");
+
     }
   }
 
