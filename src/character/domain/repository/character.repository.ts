56abb1fb461193c4a244b54336b3f74b367793da
alberto8812 <cvelascosaavedra@gamil.com>
2024-12-22 22:21:38 -0500

    feat(character): add error handling and logging to character service and repository methods

--- src/character/domain/repository/character.repository.ts
+++ src/character/domain/repository/character.repository.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { BadRequestException, Inject, Injectable, Logger } from "@nestjs/common";
 import { CrudCharacterRepository } from "./characert-repository.interface";
 import { OrmBasicReportsRepository } from "src/db/domain/repository/charcters/orm-characters.repositor";
 import { IOrmCharacterRepository } from "src/db/domain/repository/charcters/orm-characters.repositor.interface";
@@ -7,22 +7,37 @@ import { SearchFilterCharacter } from "../interface/searchFilterCharacer.interfa
 
 @Injectable()
 export class CharacterRepository implements CrudCharacterRepository {
-
+    private readonly logger = new Logger('CharacterRepository');
     constructor(
         @Inject(OrmBasicReportsRepository)
         private readonly ormBasicReportsRepository: IOrmCharacterRepository
     ) { }
     createCharacter(newCharacter: ISaveCharacterRepositoryModel): Promise<{ [key: string]: string; }> {
-        return this.ormBasicReportsRepository.saveCharacter(newCharacter);
+        try {
+            return this.ormBasicReportsRepository.saveCharacter(newCharacter);
+        } catch (error) {
+            this.logger.error(error);
+            throw new BadRequestException("Error creating character");
+        }
     }
     getAllCharacter(searchfilter: SearchFilterCharacter): Promise<CharacterRepositoryModel[]> {
-        return this.ormBasicReportsRepository.getAllCharacters(searchfilter);
+        try {
+            return this.ormBasicReportsRepository.getAllCharacters(searchfilter);
+        } catch (error) {
+            this.logger.error(error);
+            throw new BadRequestException("Error getting characters");
+        }
     }
     deleteCharacerById(employeeid: string): Promise<{ [key: string]: string; }> {
         return this.ormBasicReportsRepository.deleteCharacter(employeeid);
     }
     getCharacterById(characterId: string): Promise<CharacterRepositoryModel> {
-        return this.ormBasicReportsRepository.getCharacterById(characterId);
+        try {
+            return this.ormBasicReportsRepository.getCharacterById(characterId);
+        } catch (error) {
+            this.logger.error(error);
+            throw new BadRequestException("Error getting character");
+        }
     }
     updateCharacter(updateCharcter: CharacterRepositoryModel): Promise<{ [key: string]: string; }> {
         const { id, ...rest } = updateCharcter;
